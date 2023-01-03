923bc2701aa11dbd2c4019ce12f65b772a6c52d6
Berkay <berkaymehmetsert@gmail.com>
2023-01-03 14:55:51 +0300

    Create project

---
+++ src/test/java/com/bms/parkingapp/ParkingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bms.parkingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
